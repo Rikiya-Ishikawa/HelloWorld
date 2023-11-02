bd1c858ffa8ba077e81db852898a251594c092ac
Rikiya <d4c.raise.ability@gmail.com>
2023-11-03 08:03:10 +0900

    新規作成

---
+++ src/main/java/com/HelloWorld/HelloWorld/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.HelloWorld.HelloWorld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!!";
+    }
+
+}
